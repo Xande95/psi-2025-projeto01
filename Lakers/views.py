9789cd3f668a38ca0b8df7a15f59c0ae838cae9f
Alexandre <josealexandredapaixaodias551@gmail.com>
2025-07-04 17:31:16 -0300

    100% atualizado

--- Lakers/views.py
+++ Lakers/views.py
@@ -5,6 +5,22 @@ def index(request):
 
 def elenco(request):
     return render(request, "Lakers/elenco.html")
+    jogadores{
+        "id": 1,
+        "Nome": "LeBron James",
+        "Posição": "Ala-Pivô",
+        'Número': "23",
+
+        "id": 2,
+        "Nome": "Luka Doncit",
+        "Posição": "Armador",
+        'Número': "77",
+
+        "id": 3,
+        "Nome": "Austin Reaves",
+        "Posição": "Armador",
+        'Número': "15",
+    }
 
 def sobre(request):
     return render(request, "Lakers/sobre.html")
