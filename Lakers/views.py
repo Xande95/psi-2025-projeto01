80da78a35ad3b627d535d7a2855d75323e39335c
Paulo Gabriel <96998821+Pauloglf@users.noreply.github.com>
2025-07-06 18:34:31 -0300

    avanço mínimo

--- Lakers/views.py
+++ Lakers/views.py
@@ -4,34 +4,35 @@ def index(request):
     return render(request, "Lakers/index.html")
 
 
-jogadores = [ 
-    {
-        "id": 1,
-        "Nome": "LeBron James",
-        "Posição": "Ala-Pivô",
-        'Número': "23",
-        "foto": 0,
-        },
-    {
-        "id": 2,
-        "Nome": "Luka Doncit",
-        "Posição": "Armador",
-        'Número': "77",
-        "foto":0,
-        },
-    {
-        "id": 3,
-        "Nome": "Austin Reaves",
-        "Posição": "Armador",
-        'Número': "15",
-        "foto":0,
-    },
-]
+#jogadores = [ 
+ #   {
+  #      "id": 1,
+  #      "Nome": "LeBron James",
+ #       "Posição": "Ala-Pivô",
+   #     'Número': "23",
+  #      "foto": 0,
+ #   {
+ #       "id": 2,
+  #      "Nome": "Luka Doncit",
+  #      "Posição": "Armador",
+  #      'Número': "77",
+  #      "foto":0,
+ #       },
+ #   {
+ #       "id": 3,
+  #      "Nome": "Austin Reaves",
+ #       "Posição": "Armador",
+ #       'Número': "15",
+ #       "foto":0,
+ #   },
+#]
 
 
 def jogadores(request):
-    return render(request, "Lakers/jogadores.html")
-    
+#   context = {
+#        'time' = jogadores,
+#   }
+    return render(request, "Lakers/jogadores.html", context)
 
 
 def sobre(request):
