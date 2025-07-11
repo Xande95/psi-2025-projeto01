8ba0d9780d80bc24df07292abf31630d1135ca83
Alexandre <josealexandredapaixaodias551@gmail.com>
2025-07-11 16:16:26 -0300

    foto do minino

--- Lakers/views.py
+++ Lakers/views.py
@@ -15,7 +15,7 @@ def jogadores(request):
         "Nome": "LeBron James",
         "Posição": "Ala-Pivô",
         'Número': "23",
-        "foto": 0,}
+        "foto": "static/Lakers/img/LebBronJames.jpg",}
     {
         "id": 2,
         "Nome": "Luka Doncit",
@@ -80,37 +80,7 @@ def jogadores(request):
     ,]
 
 
-    
 
-lista_jogadores = [ 
-    {
-        "id": 1,
-        "Nome": "LeBron James",
-        "Posição": "Ala-Pivô",
-        'Número': "23",
-        "foto": "static/Lakers/img/LebBronJames.jpg",
-    },{
-        "id": 2,
-        "Nome": "Luka Doncit",
-        "Posição": "Armador",
-        'Número': "77",
-        "foto":0,
-        },
-    {
-        "id": 3,
-        "Nome": "Austin Reaves",
-        "Posição": "Armador",
-        'Número': "15",
-        "foto":0,
-    },
-]
-
-
-def jogadores(request):
-    context = {
-        'lista_jogadores': lista_jogadores,
-        }
-    return render(request, "Lakers/jogadores.html", context)
 
 
 def sobre(request):
